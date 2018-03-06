611a95adbb402224caeda0c8a2a3676476ca5137
laola1technik <laola1technik@users.noreply.github.com>
2018-03-06 16:38:11 +0100

    Set id to textview.

---
+++ app/src/androidTest/java/at/laola1/newsreader/MainActivityEspressoTest.java
@@ -0,0 +1,33 @@
+package at.laola1.newsreader;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityEspressoTest {
+
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void ensureTextChangesWork() {
+        // Type text and then press the button.
+        onView(withId(R.id.textView)).check(matches(withText("Hello World! edited")));
+    }
+
+
+}
--- app/src/main/java/at/laola1/newsreader/MainActivity.java
+++ app/src/main/java/at/laola1/newsreader/MainActivity.java
@@ -2,6 +2,7 @@ package at.laola1.newsreader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TextView textView = findViewById(R.id.textView);
+
+        CharSequence text = textView.getText();
+
+        textView.setText(text + " edited");
     }
 }
