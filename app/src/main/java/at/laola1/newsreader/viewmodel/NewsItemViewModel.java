75f21106f73db6cc4fae15bcf6cac09b95a37b59
laola1technik <laola1technik@users.noreply.github.com>
2018-03-20 16:35:16 +0100

    Creates NewsItemViewModel.

---
+++ app/src/main/java/at/laola1/newsreader/viewmodel/NewsItemViewModel.java
@@ -0,0 +1,23 @@
+package at.laola1.newsreader.viewmodel;
+
+public class NewsItemViewModel {
+
+    private String imageUrl;
+    private String title;
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
