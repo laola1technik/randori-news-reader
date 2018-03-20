af3d58099149bc32a3f4a97bec051c51c8dd7fd3
laola1technik <laola1technik@users.noreply.github.com>
2018-03-20 16:49:41 +0100

    Creates ViewHolder for News Item.

---
+++ app/src/main/java/at/laola1/newsreader/viewholder/NewsItemViewHolder.java
@@ -0,0 +1,21 @@
+package at.laola1.newsreader.viewholder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import at.laola1.newsreader.R;
+
+public class NewsItemViewHolder extends RecyclerView.ViewHolder {
+
+    private final ImageView imageView;
+    private final TextView textView;
+
+    public NewsItemViewHolder(View itemView) {
+        super(itemView);
+
+        imageView = itemView.findViewById(R.id.newsImage);
+        textView = itemView.findViewById(R.id.newsTitle);
+    }
+}
