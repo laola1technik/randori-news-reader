9285d86e0b63d53dac40d06805bbf93195562fe9
laola1technik <laola1technik@users.noreply.github.com>
2018-03-29 15:04:24 +0200

    Sets title of news item.

--- app/src/androidTest/java/at/laola1/newsreader/MainActivityEspressoTest.java
+++ app/src/androidTest/java/at/laola1/newsreader/MainActivityEspressoTest.java
@@ -10,26 +10,22 @@ import org.junit.runner.RunWith;
 import at.laola1.newsreader.overview.NewsOverviewActivity;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityEspressoTest {
 
-
+    // https://stackoverflow.com/questions/31394569/how-to-assert-inside-a-recyclerview-in-espresso
     @Rule
     public ActivityTestRule<NewsOverviewActivity> mActivityRule =
             new ActivityTestRule<>(NewsOverviewActivity.class);
 
     @Test
-    public void ensureTextChangesWork() {
-        // Type text and then press the button.
-        onView(withId(R.id.textView)).check(matches(withText("Hello World! edited")));
+    public void shouldSetNewsItemTitle() {
+        onView(withId(R.id.recyclerView)).check(matches(hasDescendant(withText("title"))));
     }
-
-
 }
