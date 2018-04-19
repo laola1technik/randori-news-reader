f348439b555ab4152ea0374e5c9581ec700ee095
laola1technik <laola1technik@users.noreply.github.com>
2018-04-19 16:04:16 +0200

    Extracts test data from production code.

--- app/src/androidTest/java/at/laola1/newsreader/MainActivityEspressoTest.java
+++ app/src/androidTest/java/at/laola1/newsreader/MainActivityEspressoTest.java
@@ -8,6 +8,10 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import at.laola1.newsreader.overview.NewsItemViewModel;
 import at.laola1.newsreader.overview.NewsOverviewActivity;
 
 import static android.support.test.espresso.Espresso.onView;
@@ -26,12 +30,42 @@ public class MainActivityEspressoTest {
             new ActivityTestRule<>(NewsOverviewActivity.class);
 
     @Test
-    public void shouldSetNewsItemTitle() {
+    public void shouldSetNewsItemTitle() throws Throwable {
+        mActivityRule.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                List<NewsItemViewModel> newsItems = new ArrayList<>();
+                NewsItemViewModel itemModel = new NewsItemViewModel();
+                itemModel.setTitle("title1");
+                itemModel.setImageUrl("https://www.laola1.at/images/redaktion/images/Fussball/Bundesliga/Rapid/korkmaz-karriere_eed54_f_940x529.jpg");
+                newsItems.add(itemModel);
+
+                mActivityRule.getActivity().setNewsItems(newsItems);
+            }
+        });
+
         onView(withId(R.id.recyclerView)).check(matches(hasDescendant(withText("title1"))));
     }
 
     @Test
-    public void shouldSetMultipleNewsItemTitles() {
+    public void shouldSetMultipleNewsItemTitles() throws Throwable {
+        mActivityRule.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                List<NewsItemViewModel> newsItems = new ArrayList<>();
+                NewsItemViewModel itemModel = new NewsItemViewModel();
+                itemModel.setTitle("title1");
+                itemModel.setImageUrl("https://www.laola1.at/images/redaktion/images/Fussball/Bundesliga/Rapid/korkmaz-karriere_eed54_f_940x529.jpg");
+                newsItems.add(itemModel);
+                NewsItemViewModel itemModel2 = new NewsItemViewModel();
+                itemModel2.setTitle("title2");
+                itemModel2.setImageUrl("https://www.laola1.at/images/redaktion/images/Fussball/Bundesliga/Rapid/korkmaz-karriere_eed54_f_940x529.jpg");
+                newsItems.add(itemModel2);
+
+                mActivityRule.getActivity().setNewsItems(newsItems);
+            }
+        });
+
         onView(withId(R.id.recyclerView)).check(matches(Matchers.allOf(
                 hasDescendant(withText("title1")),
                 hasDescendant(withText("title2"))
--- app/src/main/java/at/laola1/newsreader/overview/NewsOverviewActivity.java
+++ app/src/main/java/at/laola1/newsreader/overview/NewsOverviewActivity.java
@@ -33,8 +33,14 @@ public class NewsOverviewActivity extends AppCompatActivity {
     }
 
     private void loadContent() {
-        adapter.setNewsItems(getNewsItems());
+        List<NewsItemViewModel> newsItems = getNewsItems();
+        setNewsItems(newsItems);
     }
+
+    public void setNewsItems(List<NewsItemViewModel> newsItems) {
+        adapter.setNewsItems(newsItems);
+    }
+
 
     @NonNull
     private List<NewsItemViewModel> getNewsItems() {
@@ -42,11 +48,13 @@ public class NewsOverviewActivity extends AppCompatActivity {
         NewsItemViewModel itemModel = new NewsItemViewModel();
         itemModel.setTitle("title1");
         itemModel.setImageUrl("https://www.laola1.at/images/redaktion/images/Fussball/Bundesliga/Rapid/korkmaz-karriere_eed54_f_940x529.jpg");
-        newsItems.add(itemModel);
+        //newsItems.add(itemModel);
         NewsItemViewModel itemModel2 = new NewsItemViewModel();
         itemModel2.setTitle("title2");
         itemModel2.setImageUrl("https://www.laola1.at/images/redaktion/images/Fussball/Bundesliga/Rapid/korkmaz-karriere_eed54_f_940x529.jpg");
-        newsItems.add(itemModel2);
+        //newsItems.add(itemModel2);
         return newsItems;
     }
+
+
 }
