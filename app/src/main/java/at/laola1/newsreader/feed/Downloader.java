2f4d47c98d0c736440f07627bd0b724478b5a9a7
laola1technik <laola1technik@users.noreply.github.com>
2018-04-19 17:01:35 +0200

    Red: Download empty file.

---
+++ app/src/main/java/at/laola1/newsreader/feed/Downloader.java
@@ -0,0 +1,14 @@
+package at.laola1.newsreader.feed;
+
+
+public class Downloader {
+    private String url;
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public byte[] getContent() {
+        return null;
+    }
+}
---
+++ app/src/test/java/at/laola1/newsreader/DownloaderUnitTest.java
@@ -0,0 +1,55 @@
+package at.laola1.newsreader;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import at.laola1.newsreader.feed.Downloader;
+
+import static org.junit.Assert.assertEquals;
+
+public class DownloaderUnitTest {
+    private HttpServer server;
+
+    @Test
+    public void shouldDownloadEmptyFile() throws IOException {
+        startServer();
+        try {
+            Downloader downloader = new Downloader();
+            downloader.setUrl("http://127.0.0.1:8801/test");
+            byte[] content = downloader.getContent();
+            assertEquals(0, content.length);
+        } finally {
+            stopServer();
+        }
+    }
+
+    private void stopServer() {
+        server.stop(0);
+    }
+
+
+    public void startServer() throws IOException {
+        server = HttpServer.create(new InetSocketAddress(8801), 0);
+        server.createContext("/test", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
