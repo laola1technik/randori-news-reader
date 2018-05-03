e8df7ee57894b282d91cfa5413483fe9c0738be0
laola1technik <laola1technik@users.noreply.github.com>
2018-05-03 16:05:15 +0200

    Downloads File with text.

--- app/src/test/java/at/laola1/newsreader/DownloaderUnitTest.java
+++ app/src/test/java/at/laola1/newsreader/DownloaderUnitTest.java
@@ -29,6 +29,18 @@ public class DownloaderUnitTest {
         }
     }
 
+    @Test
+    public void shouldDownloadFileWithText() throws IOException {
+        startServer();
+        try {
+            Downloader downloader = new Downloader("http://127.0.0.1:8801/fileWithText");
+            String content = downloader.getContent();
+            assertEquals("text", content);
+        } finally {
+            stopServer();
+        }
+    }
+
     private void stopServer() {
         server.stop(0);
     }
@@ -36,12 +48,13 @@ public class DownloaderUnitTest {
 
     public void startServer() throws IOException {
         server = HttpServer.create(new InetSocketAddress(8801), 0);
-        server.createContext("/test", new MyHandler());
+        server.createContext("/test", new EmptyFileHandler());
+        server.createContext("/fileWithText", new FileWithTextHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
     }
 
-    static class MyHandler implements HttpHandler {
+    static class EmptyFileHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
             String response = "";
@@ -51,4 +64,16 @@ public class DownloaderUnitTest {
             os.close();
         }
     }
+
+    static class FileWithTextHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "text";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
 }
